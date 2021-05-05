5e12091485b40b0a4893d3a5babfe565832cf67c
sumin <shohom38@naver.com>
2021-05-05 18:39:29 +0900

    slice method practice #1

---
+++ codetest/js/sliceMethod.js
@@ -0,0 +1,9 @@
+const animal = ['dog', 'cat', 'duck', 'camel', 'lion', 'hippo', 'rabbit'];
+
+console.log(animal);
+console.log(animal.slice(3, 4)); // 3번쨰 인수인 camel이 출력된다
+console.log(animal.slice(3));
+console.log(animal.slice(5));
+console.log(animal.slice(4, 7));
+
+document.write(animal.slice(5, 7));
